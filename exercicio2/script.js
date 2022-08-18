d4a19504d047539d9f327476975c0c159a47aec6
Carlos Eduardo <eduardo-1942@hotmail.com>
2022-08-18 20:30:19 -0300

    após feedback

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,8 +1,5 @@
 const numero = Number(prompt("Diga um número para multiplicar"))
 const multi = [1,2,3,4,5,6,7,8,9,10]
-// for(let i=0; i <= 10; i++){
-//     console.log(i*numero)
-// }
 
 for(let i in multi){
     console.log(multi[i] * numero)
